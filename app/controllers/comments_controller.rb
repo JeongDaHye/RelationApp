58b765592eb5e60cc9644fe256c3fc247347d6f0
EC2 Default User <ec2-user@ip-172-31-14-99.us-east-2.compute.internal>
2018-09-28 06:59:06 +0000

    eleventh commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
   def create
-    
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.save
+    redirect_to @comment.commentable
   end 
   
   def destroy
-    
+    @comment.destroy
+    redirect_back fallback_location: root_url
+  end
+  
+  private
+  def set_comment
+     @comment = Comment.find(params[:id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:comment, :commentable_id, :commentable_type)
   end
 end
